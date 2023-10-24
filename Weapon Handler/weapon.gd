d50bcd9c10a723c666022498b85a02563c183403
jungaboon <70823690+jungaboon@users.noreply.github.com>
2023-10-24 10:59:37 -0400

    Update weapon.gd

--- Weapon Handler/weapon.gd
+++ Weapon Handler/weapon.gd
@@ -1,8 +1,10 @@
 extends Node
 class_name Weapon
 
+# This just adds a set of baseline stats for ranged weapons
+# Customize to suit your game
+
 @export var weapon_name : String
-@export var ranged : bool = true
 @export var max_mag_size : int = 18
 @export var ammo_used_per_shot : int = 1
 @export var ammo : Ammo
