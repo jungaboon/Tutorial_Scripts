3259c6d048519f05be634f5dde95e2e669d68350
jungaboon <70823690+jungaboon@users.noreply.github.com>
2023-10-24 10:17:56 -0400

    Update and rename gun.gd to recoil_handler.gd
    Tweaked code to allow for manually setting the recoil target

--- FPS Weapon Sway and Movement/recoil_handler.gd
+++ FPS Weapon Sway and Movement/recoil_handler.gd
@@ -1,31 +1,35 @@
-extends Node3D
+extends Node
+class_name RecoilHandler
 
+@export var target_object : Node3D
 @export var recoil_rotation_x : Curve
 @export var recoil_rotation_z : Curve
 @export var recoil_position_z : Curve
 @export var recoil_amplitude := Vector3(1,1,1)
 @export var lerp_speed : float = 1
+@export var recoil_speed : float = 1
 
 @export var camera_shake : CameraShake
 @export var camera_shake_amount : float = 0.3
 
+var def_pos : Vector3
+var def_rot : Vector3
 var target_rot : Vector3
 var target_pos : Vector3
 var current_time : float
 
 func _ready():
-	target_rot.y = rotation.y
+	def_pos = target_object.position
+	def_rot = target_object.rotation
+	target_rot.y = target_object.rotation.y
 	current_time = 1
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("fire"):
-		apply_recoil()
-	
 	if current_time < 1:
-		current_time += delta
-		position.z = lerp(position.z, target_pos.z, lerp_speed * delta)
-		rotation.z = lerp(rotation.z, target_rot.z, lerp_speed * delta)
-		rotation.x = lerp(rotation.x, target_rot.x, lerp_speed * delta)
+		current_time += delta * recoil_speed
+		target_object.position.z = lerp(target_object.position.z, def_pos.z + target_pos.z, lerp_speed * delta)
+		target_object.rotation.z = lerp(target_object.rotation.z, def_rot.z + target_rot.z, lerp_speed * delta)
+		target_object.rotation.x = lerp(target_object.rotation.x, def_rot.x + target_rot.x, lerp_speed * delta)
 		
 		target_rot.z = recoil_rotation_z.sample(current_time) * recoil_amplitude.y
 		target_rot.x = recoil_rotation_x.sample(current_time) * -recoil_amplitude.x
@@ -34,7 +38,7 @@ func _physics_process(delta):
 func apply_recoil():
 	camera_shake.add_trauma(camera_shake_amount)
 	recoil_amplitude.y *= -1 if randf() > 0.5 else 1
-	target_rot.z = recoil_rotation_z.sample(0)
-	target_rot.x = recoil_rotation_x.sample(0)
-	target_pos.z = recoil_position_z.sample(0)
+	target_rot.z = recoil_rotation_z.sample(0) * recoil_amplitude.y
+	target_rot.x = recoil_rotation_x.sample(0) * -recoil_amplitude.x
+	target_pos.z = recoil_position_z.sample(0) * recoil_amplitude.z
 	current_time = 0
